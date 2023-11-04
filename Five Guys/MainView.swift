e43858bde78266974abc08ab29caaa0d3b0c6135
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-11-04 20:58:19 +0100

    Project cleaned
    Removed all files

--- Five Guys/MainView.swift
+++ Five Guys/MainView.swift
@@ -8,36 +8,8 @@
 import SwiftUI
 
 struct MainView: View {
-    @ObservedObject var handler: PageHandler = PageHandler()
-    
     var body: some View {
-        ZStack {
-            Foreground()
-            ZStack {
-                Background().zIndex(0)
-                HStack {
-                    switch handler.page {
-                    case .home:
-                        HomeView()
-                            .transition(.move(edge: .leading).animation(.linear(duration: 2)))
-                    case .game:
-                        GameView()
-                            .transition(.move(edge: .trailing).animation(.linear(duration: 2)))
-                    case .history:
-                        ScoreView()
-                            .transition(.move(edge: .trailing).animation(.linear(duration: 2)))
-                    }
-                }.environmentObject(handler)
-                    .zIndex(1)
-                    .blendMode(.destinationOut)
-            }.compositingGroup()
-        }.onAppear {
-            UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
-            
-            AppDelegate.orientationLock = .portrait
-        }.onDisappear {
-            AppDelegate.orientationLock = .all
-        }
+        Text("Flippy Tiles")
     }
 }
 
